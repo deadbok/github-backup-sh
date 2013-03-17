bb59e84951cd7beb0753b9a4ab02bbc902d6a4c2
deadbok <oblivion@localhost>
2013-03-17 12:21:59 +0100

    Added target directory to the command line.

--- github-backup.sh
+++ github-backup.sh
@@ -5,19 +5,21 @@
 # Usage: github-backup.sh <username>
 # @author Petr Trofimov <petrofimov@yandex.ru>
 #
-
+# Added directory specification by deadbok
 set -ex
 
 USER="$1"
+DIR="$2"
+
 API_URL="https://api.github.com/users/${USER}/repos?type=owner"
 DATE=$(date +"%Y%m%d")
-TEMP_DIR="github_${USER}_${DATE}"
-BACKUP_FILE="${TEMP_DIR}.tgz"
+TEMP_DIR="/tmp/github_${USER}_${DATE}"
+BACKUP_FILE="${DIR}/github_$user_${DATE}.tgz"
 
-mkdir "$TEMP_DIR" && cd "$TEMP_DIR"
+mkdir -p "$TEMP_DIR" && cd "$TEMP_DIR"
 curl -s "$API_URL" | grep -Eo '"git_url": "[^"]+"' | awk '{print $2}' | xargs -n 1 git clone
 cd -
 tar zcf "$BACKUP_FILE" "$TEMP_DIR"
-rm -rf "$TEMP_DIR"
+rm -Rf "$TEMP_DIR"
 
 
